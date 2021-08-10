3586b46afa3b95cbaa573380d8f693c3508038f4
Mary Khachatryan <marykhachatryan13@gmail.com>
2021-08-10 15:24:03 +0400

    .

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,44 +7,50 @@ import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import PhoneIcon from "@material-ui/icons/Phone";
 import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const NavBar = () => (
-  <Typography className="nav">
-    <Link href="#">
-      <img src={Logo} alt="logo" />
-    </Link>
-    <ul className="menu">
-      <li>
-        <Link href="#" color="inherit">
-          Pricing
-        </Link>
-      </li>
-      <li>
-        <Link href="#" color="inherit">
-          Order a tour
-        </Link>
-      </li>
-      <li>
-        <Link href="#" color="inherit">
-          Services
-        </Link>
-      </li>
-    </ul>
-    <ul className="info">
-      <Button startIcon={<PhoneIcon />}>
-        <a href="tel:9057669009" className="phone">
-          (905) 766 9009
-        </a>
-      </Button>
-      <Button>
-        <a href="cart" className="cart">
-          <img src={Cart} alt="cart" />
-          Cart 0
-        </a>
-      </Button>
-      <SignInBtn />
-    </ul>
-  </Typography>
+  <div>
+    <Typography className="nav">
+      <Link href="#">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <ul className="menu">
+        <li>
+          <Link href="#" color="inherit">
+            Pricing
+          </Link>
+        </li>
+        <li>
+          <Link href="#" color="inherit">
+            Order a tour
+          </Link>
+        </li>
+        <li>
+          <Link href="#" color="inherit">
+            Services
+          </Link>
+        </li>
+      </ul>
+      <ul className="info">
+        <Button startIcon={<PhoneIcon />}>
+          <a href="tel:9057669009" className="phone">
+            (905) 766 9009
+          </a>
+        </Button>
+        <Button>
+          <a href="cart" className="cart">
+            <img src={Cart} alt="cart" />
+            Cart 0
+          </a>
+        </Button>
+        <SignInBtn />
+      </ul>
+    </Typography>
+    <div className="menuIcon">
+      <MenuIcon />
+    </div>
+  </div>
 );
 
 export default NavBar;
